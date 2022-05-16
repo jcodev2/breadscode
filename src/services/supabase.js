4aff3650dc64385273e7f46417f9710a89c21b3f
Jeancarlo <jcode2006@gmail.com>
2022-05-15 19:41:48 -0500

    fix(backend): add the suppabase key

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://llljmilpvaqizsxmcrpj.supabase.co'
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
+const supabaseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxsbGptaWxwdmFxaXpzeG1jcnBqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDk2MzQxOTYsImV4cCI6MTk2NTIxMDE5Nn0.dl20h3sreexh3kTX0kEX9Rb42nY6j_S3K_3UDsiCeNU'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase

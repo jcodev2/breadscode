4c89a3c112c05e2d33aabce0fd8e8f91ba05de2b
Jeancarlo <jcode2006@gmail.com>
2022-05-15 19:27:31 -0500

    Add the router dom

---
+++ src/components/Router.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import About from '../pages/About'
+import Breads from '../pages/Breads'
+import Contact from '../pages/Contact'
+import Home from '../pages/Home'
+import NotFound from '../pages/NotFound'
+
+const Router = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/breads' element={<Breads />} />
+      <Route path='/breads/:name' element={<h1>Hola</h1>} />
+      <Route path='/about' element={<About />} />
+      <Route path='/contact' element={<Contact />} />
+      <Route path='*' element={<NotFound />} />
+    </Routes>
+  )
+}
+
+export default Router

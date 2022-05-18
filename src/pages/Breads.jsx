68507bf836226676cfbfe03cac1950cd420b8b4d
Jeancarlo <jcode2006@gmail.com>
2022-05-18 17:50:55 -0500

    feat(web): add new hero images

--- src/pages/Breads.jsx
+++ src/pages/Breads.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Container from '../components/Container'
 import GoTop from '../components/GoTop'
+import HeroImage from '../components/HeroImage'
 import Title from '../components/Title'
+import HeroImageMenu from '../public/hero-image-menu.jpg'
 
 const Breads = () => {
   return (
     <>
+      <HeroImage image={HeroImageMenu}></HeroImage>
       <Container>
         <section className='section'>
           <Title text='Breads' />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,16 +4,23 @@ import Container from '../components/Container'
 import Error from '../components/Error'
 import GoTop from '../components/GoTop'
 import HeroImage from '../components/HeroImage'
+import Logo from '../components/Logo'
 import Spinner from '../components/Spinner'
 import Title from '../components/Title'
 import useBakeries from '../hooks/useBakeries'
+import HeroImageHome from '../public/hero-image-home.jpg'
+import logo from '../public/logo.png'
 
 const Home = () => {
   const { bakery, isLoading, error } = useBakeries()
 
   return (
     <>
-      <HeroImage />
+      <HeroImage image={HeroImageHome}>
+        <div className='hero-image-content'>
+          <Logo logo={logo} />
+        </div>
+      </HeroImage>
       <Container>
         <section className='section'>
           <Title text='Do you want to know our Bakeries?' />

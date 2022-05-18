8c8077669df0360e2373b7621f92704fee3c22b2
Jeancarlo <jcode2006@gmail.com>
2022-05-18 17:47:56 -0500

    astyle(web): add new colors and background

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,35 @@
-import { Cake, Envelope, House, Info } from 'phosphor-react'
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React, { createRef, useState } from 'react'
 import logo from '../public/logo.png'
-import { handleClick } from '../utilities/functions/goTop'
 import Logo from './Logo'
-import Navigation from './Navigation'
+import { Navigation, NavigationMobile } from './Navigation'
 
 const Header = () => {
+  const [isTransparent, setIsTransparent] = useState(true)
+  const HeaderRef = createRef()
+
+  const handleScroll = () => {
+    if (HeaderRef.current) {
+      const scrollTop = window.pageYOffset || document.documentElement.scrollTop
+      const HeroImageHeight =
+        HeaderRef.current.parentElement.children[1].clientHeight
+
+      if (scrollTop > HeroImageHeight - 60) {
+        setIsTransparent(false)
+      } else {
+        setIsTransparent(true)
+      }
+    }
+  }
+
+  window.addEventListener('scroll', handleScroll)
+
   return (
-    <header className='header-desktop'>
+    <header
+      className={`header-desktop ${
+        isTransparent ? 'is-transparent' : 'is-opaque'
+      }`}
+      ref={HeaderRef}
+    >
       <Logo logo={logo} />
       <Navigation />
     </header>
@@ -18,30 +39,7 @@ const Header = () => {
 const HeaderMobile = () => {
   return (
     <header className='header-mobile'>
-      <nav className='nav'>
-        <ul className='nav-list'>
-          <li className='nav-item'>
-            <NavLink to='/' onClick={handleClick}>
-              <House size={32} weight='bold' />
-            </NavLink>
-          </li>
-          <li className='nav-item'>
-            <NavLink to='/breads' onClick={handleClick}>
-              <Cake size={32} weight='bold' />
-            </NavLink>
-          </li>
-          <li className='nav-item'>
-            <NavLink to='/about' onClick={handleClick}>
-              <Info size={32} weight='bold' />
-            </NavLink>
-          </li>
-          <li className='nav-item'>
-            <NavLink to='/contact' onClick={handleClick}>
-              <Envelope size={32} weight='bold' />
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
+      <NavigationMobile />
     </header>
   )
 }

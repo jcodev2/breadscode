8da73ca2855600ac0ca1d99b0e88f0a898067ae1
Jeancarlo <jcode2006@gmail.com>
2022-05-19 16:15:08 -0500

    feat(web): Change the structure of the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,4 @@
+import { FacebookLogo, InstagramLogo, TwitterLogo } from 'phosphor-react'
 import React from 'react'
 
 const Footer = () => {
@@ -5,18 +6,37 @@ const Footer = () => {
     <footer className='footer'>
       <div className='footer-content'>
         <div className='footer-content-left'>
-          <h3>Contact Us</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-            euismod, ipsum ut dignissim consectetur, eros augue condimentum
-            nisl, eget porta nisl ipsum eget nunc. Curabitur euismod, ipsum ut
-            dignissim consectetur, eros augue condimentum nisl, eget porta nisl
-            ipsum eget nunc.
-          </p>
+          <h3>A Creative company of Breads and Bites</h3>
+          <p>breadscode@gmail.com</p>
+          <p className='city'>Peru, Lima</p>
+          <span className='phone-number'>+1-800-555-5555</span>
+          <span className='phone-number'>+51 98765432</span>
         </div>
         <div className='footer-content-right'>
           <h3>Follow Us</h3>
-          <div className='icons'></div>
+          <div className='icons'>
+            <a
+              href='https://www.facebook.com/breadscode/'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <FacebookLogo size={32} weight='bold' />
+            </a>
+            <a
+              href='https://www.instagram.com/breadscode/'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <TwitterLogo size={32} weight='bold' />
+            </a>
+            <a
+              href='https://twitter.com/breadscode'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <InstagramLogo size={32} weight='bold' />
+            </a>
+          </div>
         </div>
       </div>
       <div className='footer-copyright'>
